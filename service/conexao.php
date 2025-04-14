7640d86ada3e2e3dbbc595cf708d8ae1204a8945
igorgabs05 <gaabxvykl@gmail.com>
2025-04-14 08:20:36 -0300

    aadadadad
    dadadadadadadadada

---
+++ service/conexao.php
@@ -0,0 +1,10 @@
+<?php
+class usePDO
+{
+   private $servename = "localhost";
+   private $username = "root";
+   private $password = "";
+   private $dbname = "banco de dados";
+   private $instance;
+}
+
